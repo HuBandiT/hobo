13480fd52413004d4f49bfc1896330b91a521cfe
HuBandiT <hubandit@gmail.com>
2024-04-24 12:07:32 +0200

    check HABTM-ness more carefully

--- hobo/lib/hobo/extensions/active_record/permissions.rb
+++ hobo/lib/hobo/extensions/active_record/permissions.rb
@@ -108,7 +108,7 @@ ActiveRecord::Associations::HasManyThroughAssociation.class_eval do
     problem_joiner=nil
     association = self.respond_to?(:proxy_association) ? proxy_association : self
     through_assoc_name = association.send(:through_reflection).name
-    is_habtm = ActiveRecord::Reflection::HasAndBelongsToManyReflection === reflection.parent_reflection.last
+    is_habtm = ActiveRecord::Reflection::HasAndBelongsToManyReflection === reflection&.parent_reflection&.last
     if !is_habtm && user && records.any? { |r|
         ja = construct_join_attributes(r)
         problem_joiner = joiner = association.owner.send(through_assoc_name).where(ja).first
